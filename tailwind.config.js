8475a9911b2a67703ed38b30d4d54a4a4df929e7
aaron <aaron.kai.hubbard@gmail.com>
2022-03-09 14:37:27 -0500

    convert svgs to components. Tweak nav more

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   // more options here
   theme: {
+    screens: {
+      xs: "399px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
     extend: {
       gridTemplateRows: {
         "project-card": "2fr 1fr",
