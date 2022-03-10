edcb2dd18f1cf1cc923a68fd2691baba654fbc50
aaron <aaron.kai.hubbard@gmail.com>
2022-03-10 10:13:12 -0500

    change how global.css is imported post-migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // tailwind.config.js
 module.exports = {
   mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
+  ],
   // more options here
   theme: {
     screens: {
